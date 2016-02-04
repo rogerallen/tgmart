dfeba82d2045196115ec453ba7312c6c13235178
Roger Allen <rallen@gmail.com>
2016-02-04 13:17:28 -0800

    adjust opus2b for publication

--- src/tgmart/opus2.clj
+++ src/tgmart/opus2.clj
@@ -39,17 +39,17 @@
   ;; via twitter, https://twitter.com/nodename/status/694989814802751488
   ;; :name "160203_203104_M.clj" :parents ["160203_163143_C.clj"] :hash -781059262 :image-hash -1949814759
   (render-animation
-   [i 0 250 1]
+   [i 0 800 1]
    (->>
     (vfrac (green-from-hsl (lerp (vconcat (gradient (offset [2.1641 -1.8963 1.3623 1.1235] (vcos (vpow (clamp -2.1979 [2.8877 -0.1482 -0.9508] [2.2707 -2.5016]) (v- pos 2.6432))))) (saturation-from-rgb (rgb-from-hsl [1.4505 -0.0455 2.0777]))) (v- (adjust-hue (vconcat (lightness-from-rgb (length pos)) 2.433) 2.0835) (adjust-hsl (alpha (t (sigmoid 1.7052))) (v- pos 2.6432))) (vabs (vround (vfrac (sigmoid (v* turbulence (lerp pos 2.0475 pos)))))))))
-    (rotate (* 2 Math/PI (/ i 250)))
-    (scale [(+ 0.5 (half-circle-sin i 1 250)) (+ 0.5 (half-circle-sin i 1 250))])
+    (rotate (* 2 Math/PI (/ i 800)))
+    (scale [(+ 0.5 (half-circle-sin i 1 800)) (+ 0.5 (half-circle-sin i 1 800))])
     (offset [-0.5 (* -0.5 (/ 9 16)) ])
     )
-     {:filename "art/opus2b/a%04d.png"
-    :width   (* (/ 1920 10) 2)
-    :height  (* (/ 1080 10) 2)
-      })
+   {:filename "art/opus2b/a%04d.png"
+    :width   (* (/ 1920 10) 5)
+    :height  (* (/ 1080 10) 5)
+    })
 
   ;; just the image
   (show
