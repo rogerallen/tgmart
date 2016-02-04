bf6b3bd0daeaca406e924e1f8c85e51ae9d1b090
Roger Allen <rallen@gmail.com>
2016-02-04 08:08:13 -0800

    upgrade to latest version

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "ttp://github.com/rogerallen/tgmart"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [twitter-api         "0.7.8"]
                  [environ             "1.0.0"]
-                 [net.mikera/clisk    "0.10.0"]
+                 [net.mikera/clisk    "0.11.0"]
                  [tentacles           "0.3.0"]
-                 [tweegeemee          "1.3.2"] ;; use localrepo
+                 [tweegeemee          "1.3.4"] ;; use localrepo
                  ])
